f0fbfcc78ff75130828a628b73c2f4ab12d641af
Patel Het <125752460+hetpatel2508@users.noreply.github.com>
2024-12-04 20:15:07 +0530

    .

--- testing/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, signup } from "../controllers/auth";
-
-const authRouter: Router = Router();
-
-authRouter.get("/login", login);
-authRouter.post("/signup", signup);
-
-export default authRouter;
